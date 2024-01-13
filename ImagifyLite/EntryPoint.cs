a0100ed990c2a67378b4e5eeca20d479e51eb13a
gmurad97 <murad.dev@bk.ru>
2024-01-13 05:43:33 +0400

    ver0.1

---
+++ ImagifyLite/EntryPoint.cs
@@ -0,0 +1,37 @@
+﻿using ImageMagick;
+using System;
+using System.IO;
+
+namespace ImagifyLite
+{
+    internal class EntryPoint
+    {
+        static void Main()
+        {
+            string inputDirectory = "input_images";
+            string outputDirectory = "output_images";
+
+            if (!Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+            }
+            if (!Directory.Exists(inputDirectory))
+            {
+                Directory.CreateDirectory(inputDirectory);
+            }
+
+            string[] inputFiles = Directory.GetFiles(inputDirectory);
+
+
+            foreach (string inputFile in inputFiles)
+            {
+                string outputFileName = Path.Combine(outputDirectory, Path.GetFileName(inputFile));
+                using (var image = new MagickImage(inputFile))
+                {
+                    image.Quality = 50;
+                    image.Write(outputFileName);
+                }
+            }
+        }
+    }
+}
